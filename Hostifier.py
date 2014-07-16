13f24f0295aedd4e88b80ef0005021094963008c
Muhammad Adeel <Chaudhary-Adeel@users.noreply.github.com>
2014-07-16 18:31:52 +0300

    Update Hostifier.py

--- Hostifier.py
+++ Hostifier.py
@@ -48,24 +48,55 @@ def banner():
 # Author: Muhammad Adeel                 #
 # Blog: http://urdusecurity.blogspot.com #
 # Mail: Chaudhary1337@gmail.com          #
+# Thanks to: Tyler Borland               #
 ==========================================''' + COLOR.die
 banner()
-# variables 
 
-Dname = raw_input('\n(Domain Name)> ')
-subdomains = raw_input('(SubDomains Filename)> ')
+#Dname = raw_input('\n(Domain Name)> ')
+subdomains = raw_input('\n(SubDomains Filename)> ')
+global varFile
+global var
+global varLength
 varFile = open(subdomains, 'r')
 var = open(subdomains, 'r')
 varLength = var.readlines()
+
+def ExecuteFunc():
+	get_input = raw_input('\n1. Single URL Scan\n2. Mutiple URLs to Scan\nSelect: ')
+	if get_input == '1':
+		Dname = raw_input('Enter Domain: ')
+		if Dname.startswith('http'):
+			exit("[-] Provide Domain Like This: host.com")
+		else:
+			print COLOR.bold + "\n[+] Starting at: {0}".format(time.ctime()) + COLOR.die + "\n"
+			Hostifier(Dname, varFile, varLength)
+			print COLOR.bold + "\n[+] Ending at: {0}".format(time.ctime()) + COLOR.die
+			raw_input('\nHit ENTER to Exit.')
+	elif get_input == '2':
+		lst = raw_input('Name of URLs List: ')
+		varLst = open(lst).readlines()
+		print COLOR.bold + "\n[+] Starting at: {0}".format(time.ctime()) + COLOR.die + "\n"
+		for line in varLst:
+			varFile2 = open(subdomains, 'r')
+			var = open(subdomains, 'r')
+			varLength2 = var.readlines()
+			Xname = line.strip()
+			Dname = Xname
+			print COLOR.bold + COLOR.green + "\n[+] Now Trying: " + COLOR.die + COLOR.bold + COLOR.red + "{0}".format(Dname) + COLOR.die
+			Hostifier(Dname, varFile2, varLength2)
+		print COLOR.bold + "\n[+] Ending at: {0}".format(time.ctime()) + COLOR.die
+		raw_input('\nHit ENTER to Exit')
+	else:
+		exit("[-] Invalid Choice")
+	return 0
 
 def Hostifier(Dname, varFile, varLength):
 	global a
 	global b
 	a = 0
 	b = 0
-	print COLOR.bold + COLOR.green + "[+] Subdomains Loaded: " + COLOR.die + str(len(varLength))
+	print COLOR.bold + COLOR.green + "[+] Subdomains Loaded: {0}".format(str(len(varLength))) + COLOR.die 
 	print COLOR.bold + "\n -- Thanks For Using Hostifier -- " + COLOR.die
-	print COLOR.bold + "[+] Starting at: {0}".format(time.ctime()) + COLOR.die + "\n"
 	for sd in varFile.read().split('\n'):
 		a = a + 1
 		varX = chr(27)
@@ -80,12 +111,9 @@ def Hostifier(Dname, varFile, varLength):
 		except:
 			pass
 	sys.stdout.write(varX + '[2K' + varX + '[G')
-	print COLOR.bold + "[+] Ending at: {0}".format(time.ctime()) + COLOR.die
-	print COLOR.green + COLOR.bold + "[+] Total Results: {0}".format(b) + COLOR.die
-	varFile.close()
+	print COLOR.green + COLOR.bold + "\n[+] Total Results: {0}".format(b) + COLOR.die
 
-
-Hostifier(Dname, varFile, varLength)
+ExecuteFunc()
 		
 def main():
 	if __name__ == '__main__':
